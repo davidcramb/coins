faf027956485c721cee8a339d4c0864a420c8467
David Cramb <david.cramb@gmail.com>
2016-02-02 18:27:06 -0600

    coins now working for 0 - 100 coins, made loop to demonstrate

--- coins.js
+++ coins.js
@@ -10,22 +10,34 @@
 
 */
 
-function coinCounter (num) {
+function coinCounter(num) {
   // Initialize a JavaScript object to hold the coins
     var coinPurse = {};
-    var remainder;
     if (num => 25) {
       coinPurse.quarters = Math.floor(num/25); // calculates the floor value for quarters and inserts it into Object
-      remainder = (num - (coinPurse.quarters*25)); // returns number of coins left
+      num = (num - (coinPurse.quarters*25)); // returns number of coins left sans quarters
+    }
+    if (num >= 10 || num <= 20){ // 
+      coinPurse.dimes = Math.floor(num/10); 
+      num = (num - (coinPurse.dimes*10));
     } 
-    console.log(coinPurse.quarters);
-    console.log(remainder)
-    // remainder = num -
+    if (num >= 5) {
+      coinPurse.nickels = Math.floor(num/5);
+      num = (num - (coinPurse.nickels*5));
+    } else if (num < 5) {
+      coinPurse.nickles = 0;
+    }
+    coinPurse.pennies = num; // the remainder goes into pennies
+    return coinPurse
 }
-coinCounter(75);    
+  
+var coins = coinCounter(99) // testing condition
+console.log(coins) // shows the coinpurse object
 
-  // return coinPurse;
-// }
-
-var coins = coinCounter()
-console.log();
+// Made this loop to test if the code would display properly for 0 - 100 coins
+for (var i = 100; i >= 0; i--) {
+  coins = coinCounter(i);
+  console.log(i + " makes this many coins");
+  console.log(coins); // apparently I can't pass i and coins in the same console log without js freaking out
+} // that's like such a js thing to do
+// ikr
